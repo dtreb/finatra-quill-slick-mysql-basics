86a01b23ec14e1ac2b768f45770a2552d3464d97
dtreb <dendroidua@gmail.com>
2016-08-16 12:36:33 +0300

    DB setup for CircleCI

--- sql/init.sql
+++ sql/init.sql
@@ -1,16 +1,16 @@
-CREATE DATABASE IF NOT EXISTS library;
+CREATE DATABASE IF NOT EXISTS circle_test;
 
-DROP TABLE IF EXISTS library.book;
+DROP TABLE IF EXISTS circle_test.book;
 
-CREATE TABLE IF NOT EXISTS library.book (
+CREATE TABLE IF NOT EXISTS circle_test.book (
   id BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
   title TEXT NOT NULL,
   author TEXT NOT NULL,
   created DATETIME
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO library.book (id, title, author, created) VALUES (1, 'Ancillary Mercy', 'Ann Leckie', NOW());
-INSERT INTO library.book (id, title, author, created) VALUES (2, 'N. K. Jemisin','The Fifth Season', NOW());
-INSERT INTO library.book (id, title, author, created) VALUES (3, 'Ken Liu', 'The Grace of Kings', NOW());
-INSERT INTO library.book (id, title, author, created) VALUES (4, 'Charles E. Gannon', 'Raising Caine', NOW());
-INSERT INTO library.book (id, title, author, created) VALUES (5, 'Fran Wilde', 'Updraft', NOW());
+INSERT INTO circle_test.book (id, title, author, created) VALUES (1, 'Ancillary Mercy', 'Ann Leckie', NOW());
+INSERT INTO circle_test.book (id, title, author, created) VALUES (2, 'N. K. Jemisin','The Fifth Season', NOW());
+INSERT INTO circle_test.book (id, title, author, created) VALUES (3, 'Ken Liu', 'The Grace of Kings', NOW());
+INSERT INTO circle_test.book (id, title, author, created) VALUES (4, 'Charles E. Gannon', 'Raising Caine', NOW());
+INSERT INTO circle_test.book (id, title, author, created) VALUES (5, 'Fran Wilde', 'Updraft', NOW());
